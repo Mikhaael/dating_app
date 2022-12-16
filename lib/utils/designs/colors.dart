ad8832e845f6e1716c61beea5620d05cd2015d6c
Mikhaeel <mikhaeelogunsanya77@gmail.com>
2022-12-16 04:54:01 +0100

    Revert "Working"
    This reverts commit a9b65e63309c574849a759f79a824f4811896093.

--- lib/utils/designs/colors.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color kPrimaryColor = Colors.red;
-const Color kButtonColor = Color.fromRGBO(231, 16, 33, 0.0);
-const Color kTextColor1 = Colors.black;
-const Color kTextFieldColor = Colors.pink;
